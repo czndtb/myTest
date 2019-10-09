99a154790b8b39bd570966a69c31df86a424d7e8
cenzhanneng <cenzhanneng@gosuncn.com>
2019-10-09 17:03:41 +0800

    commit

---
+++ src/test/java/com/czn/mytest/SpringbootNetty1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.czn.mytest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootNetty1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
